94873168fa0fdca112c31a10aca4d530959cf62d
Juan Farro <Juan.Farro@wtwco.com>
2024-12-03 16:43:14 -0700

    day_01 solution

---
+++ day_01/day_01_solution.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	. "github.com/jcfrperu/go-competitive-programming"
+	"math"
+	"slices"
+)
+
+func solutionPart01(lines []string) {
+	var left = make([]int, 0)
+	var right = make([]int, 0)
+
+	for _, line := range lines {
+		parts := Split(line, " ")
+		left = append(left, ParseInt(parts[0]))
+		right = append(right, ParseInt(parts[3]))
+	}
+
+	slices.Sort(left)
+	slices.Sort(right)
+
+	var dif float64
+	for i := 0; i < len(left); i++ {
+		dif += math.Abs(float64(left[i] - right[i]))
+	}
+
+	fmt.Printf("%d", int(dif))
+}
+
+func solutionPart02(lines []string) {
+	var left = make([]int, 0)
+	var right = make([]int, 0)
+
+	for _, line := range lines {
+		parts := Split(line, " ")
+		left = append(left, ParseInt(parts[0]))
+		right = append(right, ParseInt(parts[3]))
+	}
+
+	var rightFreq, _, _, _ = Freq(right, true)
+
+	var dif int
+	for i := 0; i < len(left); i++ {
+		dif += left[i] * rightFreq[left[i]]
+	}
+
+	fmt.Printf("%d", dif)
+}
+
+// https://adventofcode.com/2023/day/1
+func main() {
+	// part 01: using string or input file
+	//RunAdventOfCodeWithString(solutionPart01, "3   4\n4   3\n2   5\n1   3\n3   9\n3   3")
+	//RunAdventOfCodeWithFile(solutionPart01, "day_01/testcases/input-part-01.txt")
+
+	// part 02: using string or input file
+	//RunAdventOfCodeWithString(solutionPart02, "3   4\n4   3\n2   5\n1   3\n3   9\n3   3")
+	RunAdventOfCodeWithFile(solutionPart02, "day_01/testcases/input-part-02.txt")
+}
---
+++ go.mod
@@ -0,0 +1,7 @@
+module advent-of-code-2024
+
+go 1.21.5
+
+require (
+	github.com/jcfrperu/go-competitive-programming v1.2.4
+)
