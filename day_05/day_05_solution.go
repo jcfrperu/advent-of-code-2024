40408cc03c9a797ea2b59f6733da04a2c469bfac
Juan Farro <Juan.Farro@wtwco.com>
2024-12-05 01:30:07 -0700

    solution day_05 part 02 + reformat whole solution

--- day_05/day_05_solution.go
+++ day_05/day_05_solution.go
@@ -3,10 +3,47 @@ package main
 import (
 	"fmt"
 	. "github.com/jcfrperu/go-competitive-programming"
+	"sort"
 	"strings"
 )
 
 func solutionPart01(lines []string) {
+	rules, updates := readRulesAndUpdates(lines)
+
+	validUpdates := make([][]string, 0)
+	for _, update := range updates {
+		if isValidUpdate(update, rules) {
+			validUpdates = append(validUpdates, update)
+		}
+	}
+
+	sum := sumOfMiddles(validUpdates)
+	fmt.Printf("%d", sum)
+}
+
+func solutionPart02(lines []string) {
+	rules, updates := readRulesAndUpdates(lines)
+
+	invalidUpdates := make([][]string, 0)
+	for _, update := range updates {
+		if !isValidUpdate(update, rules) {
+			invalidUpdates = append(invalidUpdates, update)
+		}
+	}
+
+	fixedUpdates := make([][]string, 0)
+	for _, invalidUpdate := range invalidUpdates {
+		newUpdate := tryToFixUpdate(rules, invalidUpdate)
+		if isValidUpdate(newUpdate, rules) {
+			fixedUpdates = append(fixedUpdates, newUpdate)
+		}
+	}
+
+	sum := sumOfMiddles(fixedUpdates)
+	fmt.Printf("%d", sum)
+}
+
+func readRulesAndUpdates(lines []string) (map[string]string, [][]string) {
 	rules := make(map[string]string)
 	updates := make([][]string, 0)
 
@@ -15,31 +52,11 @@ func solutionPart01(lines []string) {
 			rules[line] = line
 		}
 		if strings.Contains(line, ",") {
-			data := Split(line, ",") //[]string
-			updates = append(updates, data)
+			update := Split(line, ",")
+			updates = append(updates, update)
 		}
 	}
-
-	validLines := make([][]string, 0)
-	for _, update := range updates {
-		valid := true
-		for j, _ := range update {
-			if !isValid(rules, update, j) {
-				valid = false
-				break
-			}
-		}
-		if valid {
-			validLines = append(validLines, update)
-		}
-	}
-
-	sum := 0
-	for _, line := range validLines {
-		sum += getMiddleAsInt(line)
-	}
-
-	fmt.Printf("%d", sum)
+	return rules, updates
 }
 
 func hasRule(item string, rules map[string]string) bool {
@@ -47,40 +64,58 @@ func hasRule(item string, rules map[string]string) bool {
 	return ok
 }
 
-func isValid(rules map[string]string, list []string, index int) bool {
+func isValidUpdate(update []string, rules map[string]string) bool {
+	valid := true
+	for j := range update {
+		if !isValidAt(j, update, rules) {
+			valid = false
+			break
+		}
+	}
+	return valid
+}
+
+func isValidAt(index int, update []string, rules map[string]string) bool {
 	// from index to right
-	item := list[index]
-	//fmt.Printf("pivot: %s\n", item)
-	for i := index + 1; i < len(list); i++ {
-		pair := item + "|" + list[i]
-		//fmt.Printf("-> %s\n", pair)
+	item := update[index]
+	for i := index + 1; i < len(update); i++ {
+		pair := item + "|" + update[i]
 		if !hasRule(pair, rules) {
 			return false
 		}
 	}
-
 	// from index to left
 	for i := index - 1; i >= 0; i-- {
-		pair := list[i] + "|" + item
-		//fmt.Printf("<- %s\n", pair)
+		pair := update[i] + "|" + item
 		if !hasRule(pair, rules) {
 			return false
 		}
 	}
-
 	return true
 }
 
-func getMiddleAsInt(lines []string) int {
-	size := len(lines)
-	middle := lines[(size-1)/2]
+func getMiddleAsInt(update []string) int {
+	middle := update[(len(update)-1)/2]
 	return ParseInt(middle)
 }
 
-func solutionPart02(lines []string) {
-	count := 0
+func tryToFixUpdate(rules map[string]string, update []string) []string {
+	newUpdate := make([]string, len(update))
+	copy(newUpdate, update)
 
-	fmt.Printf("%d", count)
+	sort.SliceStable(newUpdate, func(i, j int) bool {
+		key := newUpdate[i] + "|" + newUpdate[j]
+		return hasRule(key, rules)
+	})
+	return newUpdate
+}
+
+func sumOfMiddles(updates [][]string) int {
+	sum := 0
+	for _, update := range updates {
+		sum += getMiddleAsInt(update)
+	}
+	return sum
 }
 
 // https://adventofcode.com/2024/day/5
@@ -90,6 +125,6 @@ func main() {
 	//RunAdventOfCodeWithFile(solutionPart01, "day_05/testcases/input-part-01.txt")
 
 	// part 02: using string or input file
-	RunAdventOfCodeWithString(solutionPart02, "47|53\n97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n61|53\n97|53\n61|29\n47|13\n75|47\n97|75\n47|61\n75|61\n47|29\n75|13\n53|13\n\n75,47,61,53,29\n97,61,53,29,13\n75,29,13\n75,97,47,61,53\n61,13,29\n97,13,75,29,47")
-	//RunAdventOfCodeWithFile(solutionPart02, "day_05/testcases/input-part-02.txt")
+	//RunAdventOfCodeWithString(solutionPart02, "47|53\n97|13\n97|61\n97|47\n75|29\n61|13\n75|53\n29|13\n97|29\n53|29\n61|53\n97|53\n61|29\n47|13\n75|47\n97|75\n47|61\n75|61\n47|29\n75|13\n53|13\n\n75,47,61,53,29\n97,61,53,29,13\n75,29,13\n75,97,47,61,53\n61,13,29\n97,13,75,29,47")
+	RunAdventOfCodeWithFile(solutionPart02, "day_05/testcases/input-part-02.txt")
 }
