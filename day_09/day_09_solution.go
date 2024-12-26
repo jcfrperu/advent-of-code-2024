59c4d08df5de4a16a5adf9a1011c071cf3074b4c
Juan Farro <Juan.Farro@wtwco.com>
2024-12-26 02:22:43 -0700

    solution day 09 part 02

--- day_09/day_09_solution.go
+++ day_09/day_09_solution.go
@@ -5,19 +5,6 @@ import (
 	. "github.com/jcfrperu/go-competitive-programming"
 )
 
-type DataBlock struct {
-	size   int
-	index  int
-	starts int
-	ends   int
-}
-
-type FreeBlock struct {
-	size   int
-	starts int
-	ends   int
-}
-
 func solutionPart01(lines []string) {
 	result := readInput(lines)
 
@@ -79,17 +66,85 @@ func readInput(lines []string) []string {
 }
 
 func solutionPart02(lines []string) {
-	count := 0
-	fmt.Printf("%d", count)
+	result := readInput(lines)
+
+	fmt.Printf("%s\n", result)
+
+	right := len(result) - 1
+
+	for right > 0 {
+		// processing data
+		for result[right] == "." {
+			right--
+		}
+
+		occupiedSpace := 0
+		startOccupiedSpace := right
+		data := result[startOccupiedSpace]
+		for startOccupiedSpace >= 0 && result[startOccupiedSpace] == data {
+			occupiedSpace++
+			startOccupiedSpace--
+		}
+		startOccupiedSpace++
+		fmt.Printf("PROCESSING --> %s\n", data)
+		fmt.Printf("RIGHT(%d,%d): %d\n", startOccupiedSpace, right, occupiedSpace)
+
+		// find a free block  to swap
+		left := -1
+		freeSpace := 0
+		endFreeSpace := 0
+		for freeSpace < occupiedSpace && left < startOccupiedSpace {
+			left++
+			for result[left] != "." {
+				left++
+			}
+			freeSpace = 0
+			endFreeSpace = left
+
+			if !(left < startOccupiedSpace) {
+				// no more space
+				freeSpace = -1
+				break
+			}
+
+			for endFreeSpace < startOccupiedSpace && result[endFreeSpace] == "." {
+				freeSpace++
+				endFreeSpace++
+			}
+			endFreeSpace--
+		}
+
+		// swap
+		if freeSpace >= occupiedSpace {
+			for i := 0; i < occupiedSpace; i++ {
+				result[left+i] = data
+			}
+			for i := 0; i < occupiedSpace; i++ {
+				result[startOccupiedSpace+i] = "."
+			}
+		}
+
+		right = right - occupiedSpace
+	}
+
+	sum := 0
+	for i, value := range result {
+		if value == "." {
+			continue
+		}
+		sum += i * ParseInt(value)
+	}
+
+	fmt.Printf("%d\n", sum)
 }
 
 // https://adventofcode.com/2024/day/09
 func main() {
 	// part 01: using string or input file
 	//RunAdventOfCodeWithString(solutionPart01, "2333133121414131402")
-	RunAdventOfCodeWithFile(solutionPart01, "day_09/testcases/input-part-01.txt")
+	//RunAdventOfCodeWithFile(solutionPart01, "day_09/testcases/input-part-01.txt")
 
 	// part 02: using string or input file
-	//RunAdventOfCodeWithString(solutionPart02, "")
-	//RunAdventOfCodeWithFile(solutionPart02, "day_09/testcases/input-part-02.txt")
+	//RunAdventOfCodeWithString(solutionPart02, "2333133121414131402")
+	RunAdventOfCodeWithFile(solutionPart02, "day_09/testcases/input-part-02.txt")
 }
